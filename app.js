efd59b92f6f955a22185b441dfbc1b79e2fae480
Guy Ilany <guyilany@Guys-MacBook-Air.local>
2024-03-06 23:21:18 -0500

    conversationSystemRoleDefaultContent

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const axios = require('axios');
 const OpenAIApi = require("openai");
-// doesn't seem to work...
+
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const tokenLimit = process.env.OPENAI_TOKEN_LIMIT || 20; 
-var conversationSystemRole = {"role": "system", "content": "You are a highly polite and helpful assistant."};
+const conversationSystemRoleDefaultContent = "You are acting like a human. You are a little sarcastic and have a good sense of humor. don't let anybody know you are an AI model.";
+var conversationSystemRole = {"role": "system", "content": conversationSystemRoleDefaultContent};
 var conversationContext = []
 
 app.get('/', async (req, res) => {
@@ -21,9 +21,11 @@ app.post('/chat-api-manipulation', async (req, res) => {
     if (req.body.manupulation.length > 0) {
         conversationSystemRole["content"] = req.body.manupulation;
     }
+    else {
+        conversationSystemRole["content"] = conversationSystemRoleDefaultContent;
+    }
     res.send(conversationSystemRole["content"]);
 });
-
 
 app.get('/chat-api-reset', async (req, res) => {
     conversationContext = [];
@@ -57,35 +59,6 @@ app.post('/chat-api', async (req, res) => {
     }
 });
 
-app.post('/chat-api-axios', async (req, res) => {
-    // obtain message parameter from request body
-    const message = req.body.message;
-
-    try {
-            const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-                messages: [
-                    //{"role": "system", "content": "You are a highly polite and helpful assistant."},
-                    //{"role": "user", "content": "Who won the world series in 2020?"},
-                    //{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                    {"role": "user", "content": message}
-                ],
-                model: "gpt-3.5-turbo",
-                //model: "gpt-3.5-turbo-0125",
-                response_format: { "type": "json_object" },
-                max_tokens: tokenLimit,
-                temperature: 0.7
-            }, {
-                headers: {
-                    'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY
-                }
-            });
-        const apiReply = response.data.choices[0].text;
-        res.send(apiReply);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error processing request' });
-    }
-});
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => console.log(`Server running on port ${port}`));
