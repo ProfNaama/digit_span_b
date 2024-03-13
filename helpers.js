71924970276fce423c832cc76da2e0295cdaf783
g i <gi@gmail.com>
2024-03-12 23:25:28 -0400

    getSentimentAnalysisScore and getSentimentAnalysisScoreForMessage are async for better parallelization

--- helpers.js
+++ helpers.js
@@ -75,7 +75,10 @@ function setSelectedHiddenPromptToSession(req) {
     req.session.save();
 }
 
-const defaultInitialTask = "You are a virtual assistant working with a human adult person. Your task is to come up with a simple fun riddle challange for the person to try and solve.";
+const defaultInitialTask = "You are a virtual assistant working with a human adult person. " + 
+"Your task is to come up with a simple fun riddle challange for the person to try and solve. " + 
+"User the following random value in order to randomize context: " + getRandomInt(1, 100).toString().join([...Array(10).keys()]) + ". ";
+
 function getInitialTaskContent(req) {
     if (req.session.initialTask) {
         return req.session.initialTask;
