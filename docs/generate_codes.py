d6617301e76b6266fea0485a66f4f007f57d08f5
g i <gi@gmail.com>
2024-04-28 11:26:31 -0400

    update generate codes, generated second batch of 320 codes

--- docs/generate_codes.py
+++ docs/generate_codes.py
@@ -5,13 +5,14 @@ random.seed(12233445)
 code_len = 10
 codes_count = 320
 
-def generate_codes():
+def generate_codes(codes_count=320):
     d = list(map(str, range(10))) + [chr(ord("a")+i) for i in range(26)]
     return ["".join([d[random.randint(0, len(d)-1)]  for i in range(code_len)]) for i in range(codes_count)]
     
 
 def generate_csv_format(codes):
-    print("\n".join(codes))
+    liens = ["Your Prolific PID,{}".format(c) for c in codes]
+    print("\n".join(liens))
 
 # prapare for PostgreSQL
 def generate_postgresql_format(codes):
@@ -19,6 +20,7 @@ def generate_postgresql_format(codes):
     print(",\n".join(["(\'" + c + "\')" for c in codes]))
     print (";")
     
-codes = generate_codes()
+codes = generate_codes(codes_count * 2)
+codes = codes[codes_count:]
 generate_csv_format(codes)
 generate_postgresql_format(codes)
